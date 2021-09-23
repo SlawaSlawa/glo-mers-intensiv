a7b6a96dc89b2328acc727998de26d8a14e1f200
Slawa <slawapocht4@yandex.ru>
2021-09-23 13:53:20 +0400

    day two

---
+++ js/smoothScroll.js
@@ -0,0 +1,15 @@
+const linksHead = document.querySelectorAll('.menu-list__link');
+const mainScroll = document.querySelector('.main__scroll');
+const newArray = [...linksHead,mainScroll];
+
+newArray.forEach(link => {
+	link.addEventListener('click', (evt) => {
+		evt.preventDefault();
+		const target = evt.target;
+		const ID = target.getAttribute('href').substr(1);
+		document.getElementById(ID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});		
+	});
+});
